ecc02000714bf0679ec898dd0bca06a563d8fe8a
OliverMengich <kemeioliver2000@gmail.com>
2023-05-16 09:49:31 +0300

    Adds Pages for Billings, Invoices, Products, Reports and Settings

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,12 @@
 import { WaterDrop } from '@mui/icons-material';
 import { Box, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function LoginPage() {
     return (
         <Box>
             <Box sx={{}}>
-                <Box sx={{margin: '0', padding: '30px', height: 50, boxSizing: 'border-box', display:'flex', bgcolor: '#ccc', justifyContent: 'space-between', alignItems:'center', width: '100%', color: 'white' }}>
+                <Box sx={{margin: '0', padding: '30px', height: 50, boxSizing: 'border-box', display:'flex', bgcolor: '#fff', justifyContent: 'space-between', alignItems:'center', width: '100%', color: 'white' }}>
                     <Box>
                         <h2 style={{display: 'inline-block', color: 'black'}}>MajiUp</h2>
                         <WaterDrop style={{fontSize: 30, color: 'black'}}/>
@@ -22,13 +23,16 @@ function LoginPage() {
                         left: '50%',
                         transform: 'translate(-50%, -50%)',
                         padding: '20px',
-                        boxSizing: 'border-box'
-
+                        boxSizing: 'border-box',
+                        textAlign: 'center',
+                        margin: 'auto 0',
                     }}>
-                        <h2 style={{color: 'white'}}>Login</h2>
+                        <h2 style={{color: 'white'}}></h2>
                         <input style={{ outline: 'none', width: '100%', height: '30px', borderRadius: '0px', border: '1px solid #ccc', padding: '5px'}} type="text" placeholder="Username" />
                         <input style={{ outline: 'none', width: '100%', height: '30px', borderRadius: '0px', border: '1px solid #ccc', padding: '5px', marginTop: '10px'}} type="password" placeholder="Password" />
-                        <Button sx={{color: 'black', bgcolor: 'white', width: '120px', borderRadius: '0', padding: '4px', marginTop: '10px'}} variant="contained">Login</Button>
+                        <Button sx={{color: 'black', bgcolor: 'white', width: '120px', borderRadius: '0', padding: '4px', marginTop: '10px'}} variant="contained">
+                            <Link to="/dashboard" style={{textDecoration: 'none', color: 'black'}}>LOGIN</Link>
+                        </Button>
 
                     </Box>
                 </Box>
---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,20 @@
+import NavigationIndex from '../components/Navigation';
+import SideNavigation from '../components/SideNavigation';
+import { Grid } from '@mui/material';
+function SettingsPage() {
+    return (
+        <Grid container spacing={2}>
+            <Grid style={{height: '100vh', background: '#1A73E8'}} item xs={2.5}>
+                <SideNavigation/>
+            </Grid>
+            <Grid item xs={9}>
+                <NavigationIndex/>
+                <Grid>
+                    <h1>Settings Page</h1>
+                </Grid>
+            </Grid> 
+        </Grid>
+    );
+}
+
+export default SettingsPage;
