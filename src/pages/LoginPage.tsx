60383986b845028329110e2b7aa46b597f04b6d3
OliverMengich <kemeioliver2000@gmail.com>
2023-05-31 12:56:03 +0300

    Changes Login page styling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,24 @@
-import { Box, Button } from '@mui/material';
+import { Box, Button, Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Visibility, } from '@mui/icons-material';
+import { useState } from 'react';
+import MajiUpIcon from '../assets/majiupicon.png'
 const FormInput={
     border:'1px solid #ccc',
     outline: 'none',
     padding: '15px 20px',
     borderRadius:'25px',
     height: 40,
-    width:'50vh',
+    width:'100%',
+    color: 'rgba(black,black,black,.7)'
+}
+const FormInput1={
+    border:'none',
+    outline: 'none',
+    padding: '15px 20px',
+    borderRadius:'25px',
+    height: '100%',
+    width:'90%',
     color: 'rgba(black,black,black,.7)'
 }
 
@@ -14,38 +26,50 @@ const FormHolder={
     width: '300px',
     height: '200px',
     position: 'absolute',
-    top: '40%',
+    top: '30%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     borderRadius: '10px',
     boxSizing: 'border-box',
     textAlign: 'center',
-    padding: '2vw',
 }
 const InputLabel={
     fontSize:'12px',
     display:'block', 
-    color: 'rgba(black,black,black,.7)'
+    color: 'rgba(black,black,black,.7)',
+    padding: '10px 0'
 }
 function LoginPage() {
+    const [isText, setIsText] = useState(false);
     return (
         <Box>
-            <Box sx={{bgcolor: '#C4C4C4'}}>
-                <Box sx={{bgcolor: '#F6F6F6',height: '100vh', textAlign: 'center', position: 'relative' }}>
+            <Box sx={{bgcolor: '#F6F6F6',height: '100vh',}}>
+                <Box src={MajiUpIcon} component='img' sx={{height: 80,width: 80, }} />
+                <Box sx={{height: '80%', textAlign: 'center', position: 'relative' }}>
                     <Box sx={FormHolder}>
                         <h2 style={{fontSize:'36px', color: 'rgba(black,black,black,.7)'}}>Welcome!</h2>
                         <Box>
                             <label style={InputLabel} htmlFor="email">Email address</label>
                             <input placeholder='admin@waziup.org' style={FormInput} type='text' />
                         </Box>
-                        <Box>
+                        <Box sx={{display: 'flex',flexDirection:'column', justifyContent:'flex-start'}}>
                             <label style={InputLabel} htmlFor="password">Password</label>
-                            <input placeholder='....' style={FormInput} type='password' />
+                            <Stack sx={{border:'1px solid #ccc', borderRadius:'25px', height: 40}} alignItems={'center'} flexDirection={'row'}>
+                                <input placeholder='...' style={FormInput1} type={isText?'text':'password'}/>
+                                <Visibility onClick={()=>setIsText(!isText)} sx={{color:'#979797', cursor: 'pointer'}} />
+                            </Stack>
                         </Box>
-                        <Button sx={{color: 'black',height: 56, bgcolor: '#E46B26', width: '90%', borderRadius: '24px', padding: '4px', marginTop: '40px'}} variant="contained">
+                        <Button sx={{color: 'black',height: 46, bgcolor: '#E46B26', width: '90%', borderRadius: '24px', padding: '4px', marginTop: '20px'}} variant="contained">
                             <Link to="/dashboard" style={{textDecoration: 'none', color: 'white'}}>LOGIN</Link>
                         </Button>
+                        <Link to="/" style={{textDecoration: 'none',padding: '10px', fontWeight:'lighter', color: '#7692DB'}}>Forgot Password?</Link>
                     </Box>
+                </Box>
+                <Box sx={{textAlign: 'center'}}>
+                    <p style={{fontFamily: 'cursive',fontWeight:'500', fontSize:12}}>&copy; Copyright 2023
+                        <span style={{margin: '0 10px'}}>.</span>
+                        <span style={{margin: '0 10px'}}>Privacy Policy</span>
+                    </p>
                 </Box>
             </Box>
         </Box>
