1b7b2a07313c3439fb33903457d4fdc302ff18c2
OliverMengich <kemeioliver2000@gmail.com>
2023-06-06 11:29:55 +0300

    Wrapps around with Overall Layout

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -1,7 +1,5 @@
-import NavigationIndex from '../components/Navigation';
-import SideNavigation from '../components/SideNavigation';
-import { Grid, Box } from '@mui/material';
-import { PublishOutlined} from '@mui/icons-material';
+import { Box } from '@mui/material';
+import { PublishOutlined, Face} from '@mui/icons-material';
 type Member={
     consumption: string,
     date: Date,
@@ -11,7 +9,7 @@ type Member={
     deviation: number
 }
 import RatingComponent from '../components/RatingComponent/Rating.component';
-import FaceSVG from '../assets/face.svg';
+// import FaceSVG from '../assets/face.svg';
 function CommunityPage() {
     const community: Member[]=[
         {
@@ -40,55 +38,45 @@ function CommunityPage() {
         },
     ]
     return (
-        <Grid container style={{background: '#F6F6F6'}} spacing={2}>
-            <Grid item  xs={12} >
-                <NavigationIndex />
-            </Grid>
-            <Grid container spacing={2}>       
-                <Grid item xs={2.5}>
-                    <SideNavigation />
-                </Grid>
-                <Grid style={{margin: '10px'}} item xs={9}>
-                    <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                        <h3 style={{fontSize: '24px',margin:'10px 0'}}>Community</h3>
-                        <p style={{color: '#fff',cursor:'pointer',fontSize: 16, borderRadius: '20px', padding:'10px 12px', display: 'flex',alignItems: 'center',backgroundColor: '#1C1B1F'}}>
-                            <PublishOutlined sx={{fontSize: 16, margin:'0 4px'}}/>
-                            Publish
-                        </p>
-                    </Box>
-                    <p style={{color: '#888992'}}>Appreciate the community to construct a sustainable water management system</p>
-                    <Box sx={{display: 'flex', alignItems:'center',  flexWrap: 'wrap'}}>
-                        {
-                            community.map((member: Member,id)=>(
-                                <Box sx={{bgcolor: '#fff', width: '30%',margin:'2% 1%', }}  key={id}>
-                                    <Box sx={{padding: '20px'}}>
-                                        <h4 style={{fontSize: '16px'}} >
-                                            {member.consumption}
-                                            <span style={{color: '#888992',fontWeight: 'lighter', marginLeft: '5%'}}>{member.date.toDateString()}</span>
-                                        </h4>
-                        
-                                        <h1 style={{fontSize: '48px'}} >
-                                            {member.litre}
-                                            <span style={{fontSize: '16px'}}>Litre</span>
-                                        </h1>
-                                        <p style={{fontSize: '16px', fontWeight: '600'}}>{member.deviation}% less than week 24</p>
-                                    </Box>
-                                    <Box sx={{bgcolor: '#E8E8E8', display: 'flex',justifyContent: 'space-evenly', alignItems:'center',padding:'2%', width: '100%'}}>
-                                        <>
-                                            <Box src={FaceSVG} component='img' sx={{width: 16}}/>
-                                            <h3 style={{fontSize: '16px', alignItems: 'center', fontWeight: '600'}}>
-                                                {member.name}
-                                            </h3>
-                                        </>
-                                        <RatingComponent rating={member.rating} />
-                                    </Box>
-                                </Box>
-                            ))
-                        }
-                    </Box>
-                </Grid>
-            </Grid>
-        </Grid>
+        <>
+            <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                <h3 style={{fontSize: '24px',margin:'10px 0'}}>Community</h3>
+                <p style={{color: '#fff',cursor:'pointer',fontSize: 16, borderRadius: '20px', padding:'10px 12px', display: 'flex',alignItems: 'center',backgroundColor: '#1C1B1F'}}>
+                    <PublishOutlined sx={{fontSize: 16, margin:'0 4px'}}/>
+                    Publish
+                </p>
+            </Box>
+            <p style={{color: '#888992'}}>Appreciate the community to construct a sustainable water management system</p>
+            <Box sx={{display: 'flex', alignItems:'center',  flexWrap: 'wrap'}}>
+                {
+                    community.map((member: Member,id)=>(
+                        <Box sx={{bgcolor: '#fff', width: '30%',margin:'2% 1%', }}  key={id}>
+                            <Box sx={{padding: '20px'}}>
+                                <h4 style={{fontSize: '16px'}} >
+                                    {member.consumption}
+                                    <span style={{color: '#888992',fontWeight: 'lighter', marginLeft: '5%'}}>{member.date.toDateString()}</span>
+                                </h4>
+                
+                                <h1 style={{fontSize: '48px'}} >
+                                    {member.litre}
+                                    <span style={{fontSize: '16px'}}>Litre</span>
+                                </h1>
+                                <p style={{fontSize: '16px', fontWeight: '600'}}>{member.deviation}% less than week 24</p>
+                            </Box>
+                            <Box sx={{bgcolor: '#E8E8E8', display: 'flex',justifyContent: 'space-evenly', alignItems:'center',padding:'2%', width: '100%'}}>
+                                <>
+                                    <Face sx={{fontSize: 16, margin:'0 4px'}}/>
+                                    <h3 style={{fontSize: '16px', alignItems: 'center', fontWeight: '600'}}>
+                                        {member.name}
+                                    </h3>
+                                </>
+                                <RatingComponent rating={member.rating} />
+                            </Box>
+                        </Box>
+                    ))
+                }
+            </Box>
+        </>
     );
 }
 
