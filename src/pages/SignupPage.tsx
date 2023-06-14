fbec2797190ab12ff401796e7d9916c67b5bf571
OliverMengich <kemeioliver2000@gmail.com>
2023-06-14 11:18:54 +0300

    Modifies the signup page

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,47 +1,117 @@
-import { WaterDrop } from '@mui/icons-material';
-import { Box, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
-import InputComponent from '../components/Input/Input.component';
-const ContainerStyle={
-    margin: '0', 
-    padding: '30px', 
-    height: 50, 
-    boxSizing: 'border-box', 
-    display:'flex', 
-    background: 'linear-gradient(to right, #fff, #040273)', 
-    justifyContent: 'space-between', 
-    alignItems:'center', 
-    width: '100%', 
-    color: 'white' 
+import { Box, Button, Stack } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import { Visibility, WaterDrop } from '@mui/icons-material';
+import { useContext, useState } from 'react';
+import { DevicesContext } from '../context/devices.context';
+
+const FormInput={
+    border:'1px solid #ccc',
+    outline: 'none',
+    padding: '15px 20px',
+    borderRadius:'25px',
+    height: 40,
+    width:'100%',
+    color: 'rgba(black,black,black,.7)'
 }
+const FormInput1={
+    border:'none',
+    outline: 'none',
+    padding: '15px 20px',
+    borderRadius:'25px',
+    height: '100%',
+    width:'90%',
+    color: 'rgba(black,black,black,.7)'
+}
+
 const FormHolder={
-    border: '2px solid #fff',
-    bgcolor: '#040273',
     width: '300px',
-    height: '280px',
+    height: '200px',
     position: 'absolute',
-    top: '40%',
+    top: '20%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     borderRadius: '10px',
     boxSizing: 'border-box',
     textAlign: 'center',
-    padding: '2vw',
 }
-const ButtonStyle={color: 'white', bgcolor: 'inherit',border: '1px solid #fff', width: '120px', borderRadius: '20px', padding: '4px'}
+const InputLabel={
+    fontSize:'12px',
+    display:'block', 
+    color: 'rgba(black,black,black,.7)',
+    padding: '10px 0'
+}
 function SignUpPage() {
+    const {setUser} = useContext(DevicesContext);
+    const [isText, setIsText] = useState(false);
+    const navigate = useNavigate();
+    const [userData,setUserData] = useState({
+        name:'',
+        password:''
+    })
+    function submitLoginHandler(e: React.SyntheticEvent){
+        e.preventDefault();
+        setUser(userData.name)
+        navigate('/dashboard')
+    }
+    function handleInputChange(e: React.FormEvent<HTMLInputElement>){
+        setUserData({...userData, [e.currentTarget.name]: e.currentTarget.value});
+    }
     return (
         <Box>
-            <Box sx={{}}>
-                <Box sx={ContainerStyle} >
-                    <Box sx={{display: 'flex', alignItems: 'center'}}>
-                        <h2 style={{color: 'black'}}>MajiUp</h2>
-                        <WaterDrop style={{fontSize: 30, color: 'black'}}/>
+            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <WaterDrop style={{fontSize: 'calc(20px + 1vw)', color: '#4592F6'}}/>
+                <h1 style={{fontSize: 'calc(20px + 1vw)', fontWeight:'normal'}}>MajiUp</h1>
+            </Box>
+            <Box sx={{bgcolor: '#F6F6F6',height: '100vh',}}>
+                {/* <Box src={MajiUpIcon} component='img' sx={{height: 80,width: 80, }} /> */}
+                <Box sx={{height: '80%', textAlign: 'center', position: 'relative' }}>
+                    <Box sx={{}}>
+                        <Box sx={{bgcolor: '#fff',height: '100vh', textAlign: 'center', position: 'relative' }}>
+                            <Box sx={FormHolder}>
+                                <h2 style={{fontSize:'36px', color: 'rgba(black,black,black,.7)'}}>Welcome!</h2>
+                                <form onSubmit={submitLoginHandler}>
+                                    <Box>
+                                        <label style={InputLabel} htmlFor="email">Email address</label>
+                                        <input name='email' onChange={handleInputChange} required placeholder='admin@waziup.org' style={FormInput} type='text' />
+                                    </Box>
+                                    <Box>
+                                        <label style={InputLabel} htmlFor="User Name">User Name</label>
+                                        <input name='email' onChange={handleInputChange} required placeholder='' style={FormInput} type='text' />
+                                    </Box>
+                                    <Box sx={{display: 'flex',flexDirection:'column', justifyContent:'flex-start'}}>
+                                        <label style={InputLabel} htmlFor="password">Password</label>
+                                        <Stack sx={{border:'1px solid #ccc', borderRadius:'25px', height: 40}} alignItems={'center'} flexDirection={'row'}>
+                                            <input name='password' onChange={handleInputChange} required placeholder='password' style={FormInput1} type={isText?'text':'password'}/>
+                                            <Visibility onClick={()=>setIsText(!isText)} sx={{color:'#979797', cursor: 'pointer'}} />
+                                        </Stack>
+                                    </Box>
+                                    <Box sx={{display: 'flex',flexDirection:'column', justifyContent:'flex-start'}}>
+                                        <label style={InputLabel} htmlFor="password">Confirm Password.</label>
+                                        <Stack sx={{border:'1px solid #ccc', borderRadius:'25px', height: 40}} alignItems={'center'} flexDirection={'row'}>
+                                            <input name='password' onChange={handleInputChange} required placeholder='confirm password' style={FormInput1} type={isText?'text':'password'}/>
+                                            <Visibility onClick={()=>setIsText(!isText)} sx={{color:'#979797', cursor: 'pointer'}} />
+                                        </Stack>
+                                    </Box>
+                                    <Button type='submit' sx={{color: 'black',height: 46, bgcolor: '#E46B26', width: '90%', borderRadius: '24px', padding: '4px', marginTop: '20px'}} variant="contained">
+                                        {/* <Link to="/dashboard" style={{textDecoration: 'none', color: 'white'}}> */}
+                                        LOGIN
+                                        {/* </Link> */}
+                                    </Button>
+                                </form>
+                                <Link to="/" style={{textDecoration: 'none',padding: '10px', fontWeight:'lighter', color: '#7692DB'}}>Forgot Password?</Link>
+                            </Box>
+                        </Box>
+                        <Box sx={{textAlign: 'center'}}>
+                            <p style={{fontFamily: 'cursive',fontWeight:'500', fontSize:12}}>&copy; Copyright 2023
+                                <span style={{margin: '0 10px'}}>.</span>
+                                <span style={{margin: '0 10px'}}>Privacy Policy</span>
+                            </p>
+                        </Box>
                     </Box>
-                    <Button sx={ButtonStyle} variant="outlined">
-                        <Link style={{textDecoration: 'none'}} to={'/'}>Login</Link>
-                    </Button>
                 </Box>
+        </Box>
+            {/* <Box sx={{}}>
+                
                 <Box sx={{bgcolor: '#fff',height: '100vh', textAlign: 'center', position: 'relative' }}>
                     <Box sx={FormHolder}>
                         <h2 style={{color: 'white'}}></h2>
@@ -54,7 +124,7 @@ function SignUpPage() {
                         </Button>
                     </Box>
                 </Box>
-            </Box>
+            </Box> */}
         </Box>
     );
 }
