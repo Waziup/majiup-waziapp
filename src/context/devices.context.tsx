12a4bd9e4fa7e68ab5c2e15829ba6cb1cf34e7a9
OliverMengich <kemeioliver2000@gmail.com>
2023-06-17 12:49:11 +0300

    Adds consumption array information

--- src/context/devices.context.tsx
+++ src/context/devices.context.tsx
@@ -3,6 +3,63 @@ import axios from 'axios';
 type Props={
     children: ReactNode
 }
+type Consumption = {
+    x: number,
+    y: number,
+}
+const data: Consumption[] = [
+    {
+        x: 0,
+        y: 20,
+    },
+    {
+        x : 1,
+        y : 21,
+    },
+    {
+        x: 2,
+        y: 23,
+    },
+    {
+        x: 3,
+        y: 24,
+    },
+    {
+        x: 4,
+        y: 23,
+    },
+    {
+        x: 5,
+        y: 22,
+    },
+    {
+        x: 6,
+        y: 22,
+    },
+    {
+        x: 7,
+        y: 18,
+    },
+    {
+        x: 8,
+        y: 19   ,
+    },{
+        x: 9,
+        y: 20,
+    },
+    {
+        x: 10,
+        y: 21,
+    },
+    {
+        x: 11,
+        y: 19,
+    },
+    {
+        x: 12,
+        y: 20,
+    },
+];
 export type Device={
     name: string,
     waterTemp: number,
@@ -10,7 +67,12 @@ export type Device={
     liters: number,
     on: boolean,
     isSelect: boolean,
-    id: string
+    id: string,
+    location?: {
+        lat: number,
+        lng: number
+    },
+    consumption: Consumption[]
 }
 type ContextValues={
     devices: Device[],
@@ -40,7 +102,8 @@ const DEVICES: Device[]=[
         liters: 300,
         on: true,
         isSelect: true,
-        id: '623052023'
+        id: '623052023',
+        consumption: data,
     },
     {
         name: 'Home Tank',
@@ -49,7 +112,9 @@ const DEVICES: Device[]=[
         liters: 490,
         on: false,
         isSelect: false,
-        id: '53425152023'
+        id: '53425152023',
+        consumption: data,
+
     },
     {
         name: 'Cattle Tank',
@@ -58,7 +123,9 @@ const DEVICES: Device[]=[
         liters: 290,
         on: true,
         isSelect: false,
-        id: '094342023'
+        id: '094342023',
+        consumption: data,
+
     },
     {
         name: 'Restaurant Tank',
@@ -67,7 +134,8 @@ const DEVICES: Device[]=[
         liters: 500,
         on: true,
         isSelect: false,
-        id:"6465t232324"
+        id:"6465t232324",
+        consumption: data,
     },
 ]
 
