20b973dfd49f169f3d89ccc87c5a7fc55c1caf71
OliverMengich <kemeioliver2000@gmail.com>
2023-07-02 16:19:03 +0300

    Modifies similar to Grid component

--- src/pages/DevicesPage.tsx
+++ src/pages/DevicesPage.tsx
@@ -4,54 +4,36 @@ import { useLocation,useNavigate } from "react-router-dom";
 import TankDetailComponent from "../components/TankDetail/TankDetail.component";
 import {NavigateBefore} from '@mui/icons-material';
 import { Box } from "@mui/material";
+function getWaterQuality(tds: number){
+    if (tds>0 && tds<300) {
+        return 'Excellent'
+    }else if(tds>300 &&tds<900){
+        return'Good'
+    }else if(tds>900){
+        return 'Poor'
+    }else{
+        return('Not satisfied');
+    }
+}
 function DevicesPage() {
     
     const data = useLocation();
     const navigate = useNavigate();
     console.log(data.state);
-    const resp= data.state.tds;
-    let foundQuality = '';
-    if (resp<300) {
-        foundQuality='Excellent'
-    }else if(resp>300 &&resp<900){
-        foundQuality='Good'
-    }else if(resp>900){
-        foundQuality='Poor'
-    }else{
-        foundQuality=('not satisfied');
-    }
+    const tdsValue= data.state.tds;
+    
     return (
         <Box pl={2} pr={2}>
            <NavigateBefore sx={{cursor:'pointer', border: '1px solid #000',borderRadius:'50%'}} onClick={()=>navigate(-1)} /> 
-           {
-                !resp ? (
-                    <Box sx={{position: 'relative'}}>
-                        <Box sx={{
-                            position: 'absolute',
-                            top: '50%',
-                            left: '5%',
-                            // transform: 'translate(-50, -50%)',
-                            marginTop: '10px'
-                        }}>
-                            <h3 style={{fontSize: '15px', textAlign: 'center', margin:'10px 0'}}>
-                                {data.state.name}
-                            </h3>
-                            {/* <Box component='img' src={FrameSVG}/> */}
-                            
-                            <p style={{color: '#888992',fontWeight: '600',textAlign: 'center', fontSize: 16}}>No readings detected for this device.</p>
-                        </Box>
-                    </Box>
-                ):(
-                    <TankDetailComponent
-                        liters={data.state.liters.filter(ltrs=>ltrs)[0]}
-                        on={data.state.on}
-                        owner={data.state.name}
-                        waterQuality={foundQuality}
-                        waterTemp={data.state.temp.filter(temp=>temp)[0]}
-                    />
-                )
-            }
-
+                <TankDetailComponent
+                    liters={data.state.liters}
+                    on={data.state.on}
+                    owner={data.state.name}
+                    waterQuality={getWaterQuality(tdsValue)}
+                    waterTemp={data.state.temp}
+                    actuator={data.state.actuators}
+                    consumption={data.state.consumption}
+                />
         </Box>
     );
 }
