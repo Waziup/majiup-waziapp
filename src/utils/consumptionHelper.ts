c47664e506d50e50c68b03437e545dd16417a55d
OliverMengich <kemeioliver2000@gmail.com>
2023-07-17 10:47:39 +0300

    Moves handle fetch function

--- src/utils/consumptionHelper.ts
+++ src/utils/consumptionHelper.ts
@@ -11,7 +11,7 @@ type ObjProps={
     liters: number, 
     [key: string]: number | string
 }
-export async function getConsumption(deviceId: string) {
+export async function getConsumption(deviceId: string, tankHeight: number, tankCapacity: number) {
     const response = await axios.get(`http://localhost:8080/tanks/${deviceId}/tank-info`, {
         headers:{
             'Accept': 'application/json',
@@ -41,7 +41,7 @@ export async function getConsumption(deviceId: string) {
                     const date = new Date(obj[property][index].timestamp)
                     newObj.time = `${date.getHours()}hr:${date.getMinutes()}mins:${date.getSeconds()}s`;
                     newObj[property] = obj[property][index].value;
-                    newObj.liters=((obj['waterLevel'][0].value)/200)*2000;
+                    newObj.liters=((obj['waterLevel'][0].value)/tankHeight)*tankCapacity;
                     // console.log(property,previousValue)
                 } else {
                     newObj[property] = obj[property][0].value;
@@ -59,4 +59,30 @@ export async function getConsumption(deviceId: string) {
 }
 export const getLiters = (waterLevel: number, tankHeight: number, tankCapacity: number) => {
     return (waterLevel / tankHeight) * tankCapacity;
+}
+export function handleFetchTableComponents(deviceId: string){
+    const getLevel = axios.get(`http://localhost:8080/tanks/${deviceId}/waterlevel/value`)
+    const getWaterQuality = axios.get(`http://localhost:8080/tanks/${deviceId}/water-quality/value`)
+    const getTemp = axios.get(`http://localhost:8080/tanks/${deviceId}/water-temperature/value`)
+
+    axios.all([getLevel,getWaterQuality,getTemp])
+    .then(axios.spread(function (respLevel,respQuality,respTemp){
+        console.log(respLevel.data,respQuality.data,respTemp.data)
+        // const lastArr = selectedTableTank.consumption[selectedTableTank.consumption.length];
+        // if (lastArr.level !==respLevel.data || lastArr.quality !== respQuality.data || lastArr.temp !== respTemp.data ){
+        //     const date = new Date();
+        //     setSelectedTableTank({
+        //         consumption: [
+        //             // ...selectedTableTank.consumption,
+        //             {
+        //                 time: `${date.getHours()}:${date.getMinutes()}`,
+        //                 litres:  selectedTableTank1.liters,
+        //                 level: isNaN(Math.round((devices[0].liters/devices[0].capacity)*100))?0:Math.round((devices[0].liters/devices[0].capacity)*100),
+        //                 quality: selectedTableTank1.tds,
+        //                 waterTemperature: selectedTableTank1.temp,
+        //             }
+        //         ]
+        //     })
+        // }
+    }))
 }
