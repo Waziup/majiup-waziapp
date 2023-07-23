17e1abfce735308300324c4b2c90681bf8557fa7
OliverMengich <kemeioliver2000@gmail.com>
2023-07-23 04:27:01 +0300

    Build for commonjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+// import requireTransform from 'vite-plugin-require-transform';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
@@ -11,5 +11,14 @@ export default defineConfig({
 		host: true, // needed for the Docker Container port mapping to work
 		strictPort: true,
 		port: 5173, // you can replace this port with any port
+	},
+	optimizeDeps: {
+		include: ['linked-dep'],
+	},
+	build: {
+		commonjsOptions: {
+			transformMixedEsModules: true
+		}
 	}
 })
+
