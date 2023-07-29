d1118b85319b20955e607910e64f5cfde64dfa9f
OliverMengich <kemeioliver2000@gmail.com>
2023-07-29 08:29:15 +0300

    Passes the devices capacity to tank detail and actuation function

--- src/pages/DevicesPage.tsx
+++ src/pages/DevicesPage.tsx
@@ -1,9 +1,10 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
 import { useLocation,useNavigate } from "react-router-dom";
 import TankDetailComponent from "../components/TankDetail/TankDetail.component";
 import {NavigateBefore} from '@mui/icons-material';
 import { Box } from "@mui/material";
+import { DevicesContext } from "../context/devices.context";
+import { useContext } from "react";
+import { X as Device } from "../context/devices.context";
 function getWaterQuality(tds: number){
     if (tds>0 && tds<300) {
         return 'Excellent'
@@ -16,12 +17,22 @@ function getWaterQuality(tds: number){
     }
 }
 function DevicesPage() {
-    
+    const {devices, setTanks} = useContext(DevicesContext);
     const data = useLocation();
     const navigate = useNavigate();
     console.log(data.state);
     const tdsValue= data.state.tds;
-    
+    function toogleActuatorHandler(id:string) {
+        const newTanks = devices.map((item: Device) => {
+            if(item.id === id){
+                item.actuators[0].value = !item.actuators[0].value;
+            }else{
+                item.isSelect = false;
+            }
+            return item;
+        })
+        setTanks(newTanks);
+    }
     return (
         <Box pl={2} pr={2}>
            <NavigateBefore sx={{cursor:'pointer', border: '1px solid #000',borderRadius:'50%'}} onClick={()=>navigate(-1)} /> 
@@ -34,6 +45,9 @@ function DevicesPage() {
                     actuator={data.state.actuators}
                     consumption={data.state.consumption}
                     height={data.state.height}
+                    capacity={data.state.capacity}
+                    id={data.state.id}
+                    toggleActuator={toogleActuatorHandler}
                 />
         </Box>
     );
