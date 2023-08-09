1572d753fb0f728be40a4c58c45fbf6e5620a147
OliverMengich <kemeioliver2000@gmail.com>
2023-08-09 13:53:40 +0300

    Navigates to wazigate, logouts and cclose

--- src/components/IconMenu/IconMenu.component.tsx
+++ src/components/IconMenu/IconMenu.component.tsx
@@ -2,32 +2,45 @@ import {Menu,Divider, MenuItem,ListItemText, ListItemIcon} from '@mui/material'
 import {Launch, Logout, AccountCircle} from '@mui/icons-material'
 import { useContext } from 'react';
 import { DevicesContext } from '../../context/devices.context';
+import {Link, useNavigate} from 'react-router-dom';
+import Fade from '@mui/material/Fade';
 interface Props {
     isOpen: boolean;
     anchorEl: null | HTMLElement;
     handleClose: () => void;
 }
 export default function IconMenuComponent({isOpen, anchorEl, handleClose}:Props) {
-    const {setUser, user} = useContext(DevicesContext)
+    const {setUser} = useContext(DevicesContext)
+    const navigate = useNavigate();
     const handleLogout = () => {
-        setUser('','')
+        setUser('','');
+        navigate('/');
         handleClose();
     }
     return (
-        <Menu anchorEl={anchorEl}  sx={{ width: 400, maxWidth: '100%', borderRadius:0 }} open={isOpen}>
+        <Menu 
+            TransitionComponent={Fade} 
+            onClose={handleClose} 
+            anchorEl={anchorEl}
+            MenuListProps={{
+                'aria-labelledby': 'fade-button',
+            }}
+            sx={{ width: 400, maxWidth: '100%', borderRadius:0 }} 
+            open={isOpen}>
             <MenuItem onClick={handleClose}>
                 <ListItemIcon color='#000'>
                     <AccountCircle fontSize="small" />
                 </ListItemIcon>
-                <ListItemText>{user.name}</ListItemText>
+                <ListItemText>Admin</ListItemText>
                 
             </MenuItem>
             <MenuItem onClick={handleClose}>
-                <ListItemIcon>
-                    <Launch fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Wazigate</ListItemText>
-                
+                <Link style={{textDecoration: 'none', display: 'flex'}} target='_blank' to='http://wazigate.local/'>
+                    <ListItemIcon>
+                        <Launch fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>Wazigate</ListItemText>
+                </Link>
             </MenuItem>
             <Divider />
             <MenuItem onClick={handleLogout}>
