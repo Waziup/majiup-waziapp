1dc72e16c3d97a48d7bd8eb577fc00f76abeafaf
OliverMengich <kemeioliver2000@gmail.com>
2023-08-10 09:33:36 +0300

    feat: default username and password

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -49,6 +49,12 @@ function LoginPage() {
     })
     function submitLoginHandler(e: React.SyntheticEvent){
         e.preventDefault();
+        const correctPass = "loragateway";
+        const correctUser = "admin";
+        if(userData.name !== correctUser || userData.password !== correctPass){
+            alert('Wrong username or password');
+            return;
+        }
         navigate('/dashboard')
     }
     function handleInputChange(e: React.FormEvent<HTMLInputElement>){
@@ -60,28 +66,26 @@ function LoginPage() {
                 <Box src={MajiUpIcon} component='img' sx={{height: 80,width: 80, }} />
                 <Box sx={{height: '80%', textAlign: 'center', position: 'relative' }}>
                     <Box sx={{}}>
-                        <Box sx={{bgcolor: '#fff',height: '100vh', textAlign: 'center', position: 'relative' }}>
+                        <Box sx={{bgcolor: '#fff',height: '80vh', textAlign: 'center', position: 'relative' }}>
                             <Box sx={FormHolder}>
                                 <h2 style={{fontSize:'36px', color: 'rgba(black,black,black,.7)'}}>Welcome!</h2>
                                 <form onSubmit={submitLoginHandler}>
                                     <Box>
-                                        <label style={InputLabel} htmlFor="email">Email address</label>
-                                        <input name='name' onChange={handleInputChange} required placeholder='admin@waziup.org' style={FormInput} type='text' />
+                                        <label style={InputLabel} htmlFor="email">User name</label>
+                                        <input name='name' onChange={handleInputChange} required placeholder='admin' style={FormInput} type='text' />
                                     </Box>
                                     <Box sx={{display: 'flex',flexDirection:'column', justifyContent:'flex-start'}}>
                                         <label style={InputLabel} htmlFor="password">Password</label>
                                         <Stack sx={{border:'1px solid #ccc', borderRadius:'25px', height: 40}} alignItems={'center'} flexDirection={'row'}>
-                                            <input name='password' onChange={handleInputChange} required placeholder='...' style={FormInput1} type={isText?'text':'password'}/>
+                                            <input name='password' onChange={handleInputChange} required placeholder='loragateway' style={FormInput1} type={isText?'text':'password'}/>
                                             <Visibility onClick={()=>setIsText(!isText)} sx={{color:'#979797', cursor: 'pointer'}} />
                                         </Stack>
                                     </Box>
-                                    <Button type='submit' sx={{color: 'black',height: 46, bgcolor: '#E46B26', width: '90%', borderRadius: '24px', padding: '4px', marginTop: '20px'}} variant="contained">
-                                        {/* <Link to="/dashboard" style={{textDecoration: 'none', color: 'white'}}> */}
+                                    <Button type='submit' sx={{color: 'white',fontWeight: 'bolder', height: 46, bgcolor: '#E46B26', width: '90%', borderRadius: '24px', padding: '4px', marginTop: '20px'}} variant="contained">
                                         LOGIN
-                                        {/* </Link> */}
                                     </Button>
                                 </form>
-                                <Link to="/" style={{textDecoration: 'none',padding: '10px', fontWeight:'lighter', color: '#7692DB'}}>Forgot Password?</Link>
+                                <Link to="/" style={{textDecoration: 'none',padding: '10px', fontWeight:'normal', color: '#7692DB'}}>Forgot Password?</Link>
                             </Box>
                         </Box>
                         <Box sx={{textAlign: 'center'}}>
