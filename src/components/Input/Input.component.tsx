847dc7d84533ee4eab79fd5320cfa5bca6990938
OliverMengich <kemeioliver2000@gmail.com>
2023-08-12 13:52:37 +0300

    feat: search fo device functionality

--- src/components/Input/Input.component.tsx
+++ src/components/Input/Input.component.tsx
@@ -1,14 +1,20 @@
-type Props={
-    placeholder: string,
-    type: string
-}
+
 import SearchIcon from '@mui/icons-material/Search';
+type Props = React.InputHTMLAttributes<HTMLInputElement>
+import React, { useContext } from 'react';
+import { DevicesContext } from '../../context/devices.context';
 import { Box } from '@mui/material';
-function InputComponent({placeholder,type}: Props) {
+function InputComponent({placeholder, type}: Props) {
+    const [search, setSearch] = React.useState<string>('');
+    const {searchDevices} = useContext(DevicesContext);
+    function handleSearch(e: React.ChangeEvent<HTMLInputElement>){
+        setSearch(e.target.value);
+        searchDevices(search);
+    }
     return (
         <>
             <Box  sx={{height: '40px',borderRadius:'20px', display: 'flex', alignItems: 'center', width: '100%', bgcolor: '#F6F6F6'}}>
-                <input style={{height: '100%',paddingLeft:'10px', outline:'none',border: 'none', background: '#F6F6F6', width: '95%', borderRadius: 'inherit'}} className="input_box" type={type} placeholder={placeholder} />
+                <input value={search} onChange={handleSearch} style={{height: '100%',paddingLeft:'10px', outline:'none',border: 'none', background: '#F6F6F6', width: '95%', borderRadius: 'inherit'}} className="input_box" type={type} placeholder={placeholder} />
                 <SearchIcon style={{color: 'black',cursor:'pointer'}}/>
             </Box>
             {/* <input style={{outline: 'none',
