a25308bc28272ea4fa04d2961ea90dfcadbd176b
josephmusya <muciajoe@gmail.com>
2023-10-10 22:22:02 +0300

    Modified the component element: Fix -> Copilot window styling

--- src/components/copilot/copilot.component.tsx
+++ src/components/copilot/copilot.component.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react"
+import React, { useState } from "react"
 import { IoMdSend } from 'react-icons/io';
 import './copilot.css';
 import { BsDashLg } from 'react-icons/bs';
@@ -8,8 +8,9 @@ const Copilot = () => {
 
     const [copilotReply, setCopilotReply] = useState();
     const [loading, setLoading] = useState(Boolean);
-
-    const queryRef = useRef<HTMLInputElement>(null);
+    const [query, setQuery] = useState('');
+    // const queryRef = useRef<HTMLInputElement>(null);
+    const [dispQ, setDispQ] = useState();
     const [showCopilot, setShowCopilot] = useState(false);
     const [error, setError] = useState(false);
 
@@ -17,15 +18,14 @@ const Copilot = () => {
         position:'absolute',
         bottom:'1%',
         left:'1%',
-        padding:'1rem',
-        borderRadius:'1rem',
+        borderRadius:`${showCopilot?'1rem':'100%'}`,
         minHeight:`${showCopilot?'40svh':'fit-content'}`,
         maxHeight:`fit-content`,
         overflow:'auto',
         display:'flex',
         // placeItems:'center',
         flexDirection:'column',
-        width:'350px',
+        width:`${showCopilot?'350px':'fit-content'}`,
         // border:'solid 1px',
         backgroundColor:'white',
         zIndex:1,
@@ -35,8 +35,9 @@ const Copilot = () => {
 
     const queryBoxStyle:React.CSSProperties = {
         position:'absolute',
-        bottom:'3%',
-        width:'90%',
+        bottom:'1%',
+        width:'100%',
+        padding:'0.5rem',
     }
 
     const inputStyle :React.CSSProperties  = {
@@ -45,29 +46,30 @@ const Copilot = () => {
         border:'none',
         borderBottom:'solid 1px',
         width:'inherit',
-        fontSize:'14px'
-        
+        fontSize:'14px'                
     } 
 
-    const answerStyle :React.CSSProperties = {
-        backgroundColor:'gray',
+    const answerStyle :React.CSSProperties = {        
+        backgroundColor:'#1976d2',
+        width:'inherit',
+        color:'#fff',
         padding:'8px',
         borderRadius:'0 12px 12px 12px',
         fontSize:'14px',
         lineHeight:1.3,
-        width:'80%',
         marginBottom:'2.5rem',
-        zIndex:1,
+        zIndex:1,   
+        // whiteSpace:'pre-line'
     }  
 
-    const askMajiupCopilot = async () => {
+    const askMajiupCopilot = async (q: any) => {
+        setQuery('');
+        setDispQ(q)
         setError(false)
         setShowCopilot(true);
         setLoading(true);
-        const query = queryRef.current?.value;
 
-        try {
-        
+        try {        
             const response = await fetch('http://localhost:8081/api/v1/ask-majiup-copilot',{
                 method:'POST',
                 headers:{
@@ -82,11 +84,10 @@ const Copilot = () => {
         } catch (err) {
             // console.log(err)
             setError(true)
-        }
-        
-        
-
+        }    
+                
         setLoading(false);
+        
     }
 
     const toggleCopilot = () => {
@@ -96,28 +97,31 @@ const Copilot = () => {
 
     return (
         <div style={copilotStyle}>
-            <strong style={{display:'flex', paddingBottom:`${(showCopilot && !copilotReply)?'2rem':''}`,justifyContent:'space-between'}}>
-                Majiup Copilot
+            <strong style={{display:'flex', padding:'0.5rem',justifyContent:'space-between', backgroundColor:'#1976d2', color:'white'}}>
+                <span className={`${showCopilot?`show`:`hide`}`}>Majiup AI Copilot</span>
                 {
                     showCopilot?<BsDashLg size={20} style={{cursor:'pointer'}} onClick={toggleCopilot} />:<RiAddFill size={20} style={{cursor:'pointer'}} onClick={toggleCopilot} />
                 }
             </strong>
             <div className={`${showCopilot?`show`:`hide`}`} style={queryBoxStyle}>
-                <input ref={queryRef} placeholder="Ask majiup copilot" style={inputStyle} type="text" />    
-                <IoMdSend size={20} style={{cursor:'pointer'}} onClick={askMajiupCopilot}/> 
+                <input value={query} onChange={(e)=>setQuery(e.target.value)} placeholder="Ask majiup copilot" style={inputStyle} type="text" />    
+                <IoMdSend color={(query)?'#1a73e8':'gray'} size={20} style={{cursor: query?`pointer`:'auto'}} onClick={()=>query && askMajiupCopilot(query)}/> 
             </div>       
             {                
-                <div style={{display:'flex', flexDirection:'column', gap:'6px'}}>
+                <div style={{display:`${showCopilot?'flex':'none'}`, padding:'0.5rem',flexDirection:'column', gap:'6px'}}>
                     {
-                        (queryRef.current?.value) && (
-                            <div style={{display:'flex', justifyContent:'flex-end'}}>
-                                <p className={`${showCopilot?`show`:`hide`}`} style={{backgroundColor:'gray', lineHeight:1.3, fontSize:'14px',maxWidth:'60%', minWidth:'50px', padding:'8px', borderRadius:'12px 0px 12px 12px'}}>
-                                    {
-                                        queryRef.current?.value
-                                    }
-                                </p>
-                            </div>
-                        )
+                        !loading && !copilotReply && showCopilot && <div>Ask majiup copilot something...</div>
+                    }
+                    {
+                        dispQ &&
+                        <div style={{display:'flex', justifyContent:'flex-end'}}>
+                            <p className={`${showCopilot?`show`:`hide`}`} style={{backgroundColor:'#bbbbbb', lineHeight:1.3, fontSize:'14px',maxWidth:'60%', minWidth:'50px', padding:'8px', borderRadius:'12px 0px 12px 12px'}}>
+                                {
+                                    dispQ
+                                }
+                            </p>
+                        </div>
+                        
                     }                    
                     {
                         loading &&
@@ -132,9 +136,12 @@ const Copilot = () => {
                     }
                     {
                         (copilotReply && !loading) &&
-                        <p className={`${showCopilot?`show`:`hide`}`} style={answerStyle}>
-                            {copilotReply}
-                        </p> 
+                        <div style={{width:'85%', overflow:'auto',}}>
+                            <p className={`${showCopilot?`show`:`hide`}`} style={answerStyle}>
+                                {copilotReply}                            
+                            </p>
+                        </div>
+                         
                     }
                     {
                         error && 
